9e00e5c481c566cdf0e1367a6dacd57ab0cdef49
Jesse Jonas <jessejonas13@gmail.com>
2021-06-14 11:19:00 -0300

    feat: interfaces e duck type

--- sprint-2/2-js-interfaces_herancas_oo/1-bytebank/Funcionarios/Diretor.js
+++ sprint-2/2-js-interfaces_herancas_oo/1-bytebank/Funcionarios/Diretor.js
@@ -1,6 +1,6 @@
 import { Funcionario } from "./Funcionario.js";
 
-export class Gerente extends Funcionario {
+export class Diretor extends Funcionario {
   constructor(nome, salario, cpf) {
     super(nome, salario, cpf); // passar para a instância superior
     this._bonificacao = 2;
--- sprint-2/2-js-interfaces_herancas_oo/1-bytebank/index.js
+++ sprint-2/2-js-interfaces_herancas_oo/1-bytebank/index.js
@@ -1,16 +1,21 @@
-import { ContaCorrente } from './Conta/ContaCorrente.js';
-import { ContaPoupanca } from './Conta/ContaPoupanca.js';
 import { Cliente } from './Conta/Cliente.js';
-import { ContaSalario } from './Conta/ContaSalario.js';
+import { Gerente } from './Funcionarios/Gerente.js';
+import { Diretor } from './Funcionarios/Diretor.js';
 
-// parametros para o constructor constructor
-const clienteHellen = new Cliente('Hellen Nuni', 19563210144);
+import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
-const contaCorrenteHellen = new ContaCorrente(0, clienteHellen, 1001);
-const contaPoupancaHellen = new ContaPoupanca(50, clienteHellen, 1001);
-const contaSalarioHellen = new ContaSalario(clienteHellen);
+const diretor = new Diretor('Hellen', 10000, 12345678900);
+diretor.cadastrarSenha('123456');
 
-contaSalarioHellen.depositar(100);
-contaSalarioHellen.sacar(10);
+const gerente = new Gerente('Nuni', 5000, 12345678901);
+gerente.cadastrarSenha('123');
 
-console.log(contaSalarioHellen);
+const cliente = new Cliente('Pogba', 78912345600, '456');
+
+const diretorLogado = SistemaAutenticacao.login(diretor, '123456');
+const gerenteLogado = SistemaAutenticacao.login(gerente, '123');
+const clienteLogado = SistemaAutenticacao.login(cliente, '456');
+
+console.log(diretorLogado);
+console.log(gerenteLogado);
+console.log(clienteLogado);
