2f8b24c2c5468a231fd5b515fb8f5f4216b04148
Jesse Jonas <jessejonas13@gmail.com>
2021-06-28 17:23:03 -0300

    feat: finaliza testes

--- sprint-4/3-testes/1976-react-testes/src/conta/Conta.js
+++ sprint-4/3-testes/1976-react-testes/src/conta/Conta.js
@@ -20,7 +20,14 @@ const Conta = ({ saldo, realizarTransacao }) => {
 
     return <div className="Conta-header">
         <h2>Conta</h2>
-        <p>Saldo: <span data-testid="saldo-conta" className="Saldo-valor">{`R$ ${saldo}`}</span></p>
+        <p>
+            Saldo: 
+            <span 
+                data-testid="saldo-conta" 
+                className="Saldo-valor">{`R$ ${saldo}`}
+            </span>
+        </p>
+        
         <form onSubmit={handleSubmit}>
             <div>
                 <label>
---
+++ sprint-4/3-testes/1976-react-testes/src/conta/Conta.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import Conta from './Conta';
+
+describe('Componente de conta', () => {
+  it('Exibir o saldo da conta com a formatação monetária', () => {
+    render(<Conta saldo={1000} />);
+
+    const saldo = screen.getByTestId('saldo-conta');
+
+    expect(saldo.textContent).toBe('R$ 1000');
+  });
+
+  it('Chama a função de realizar a transação quando o botão é clicado', () => {
+    const funcaoRealizarTransacao = jest.fn();
+    render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />);
+
+    fireEvent.click(screen.getByText('Realizar operação'));
+
+    expect(funcaoRealizarTransacao).toHaveBeenCalled();
+  })
+});
