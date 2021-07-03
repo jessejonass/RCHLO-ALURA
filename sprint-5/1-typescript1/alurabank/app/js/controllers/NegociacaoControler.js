b01073471c69b4f4a9603148dcef2a9f9811535c
Jesse Jonas <jessejonas13@gmail.com>
2021-07-03 09:00:09 -0300

    feat: add negociacoes model

---
+++ sprint-5/1-typescript1/alurabank/app/js/controllers/NegociacaoControler.js
@@ -0,0 +1,16 @@
+class NegociacaoController {
+    constructor() {
+        this._negociaocoes = new Negociacoes();
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociaocoes.adiciona(negociacao);
+        this._negociaocoes.paraArray().forEach(n => {
+            console.log(n);
+        });
+    }
+}
