7a4ff20258883e70e40f318fe53c5f01559cb804
Jesse Jonas <jessejonas13@gmail.com>
2021-07-06 13:52:45 -0300

    chore: add typescript

--- sprint-5/01-ts/bolsa/dist/js/app.js
+++ sprint-5/01-ts/bolsa/dist/js/app.js
@@ -1,6 +1,4 @@
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 1, 10);
-console.log(negociacao);
-negociacao.quantidade = 1000;
-console.log(negociacao);
+const negociacao = new Negociacao(new Date(), 100, 10);
+console.log(negociacao.volume);
